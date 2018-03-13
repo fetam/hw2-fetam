eefb771a6a854c6d784ebfaeabd26911b29db178
flortamez <flortamez@192.168.1.65>
2018-03-13 00:57:23 -0500

    I did the other homework yesterday but not this one

---
+++ Program 2/src/program2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class program2 {
+	    public static void main(String[] args) {
+	        
+	        Scanner sc = new Scanner(System.in);
+	        int tc,j,city,i,minGas;
+	        int ans=0, ans1 = 0, ans2 = 0, ans3 = 0, ans4 = 0, ans5 = 0;
+	        int ans6 = 0, ans7 =0, ans8 = 0, ans9 =0;
+	        
+	        System.out.println("Input:");
+	        tc = sc.nextInt();
+	        
+	    }
+}
